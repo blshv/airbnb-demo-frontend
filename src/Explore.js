9ca5b962ed17e0527979a66eb5df6b4dbfdc843f
blshv <awbalashov@gmail.com>
2017-11-08 00:06:47 +0300

    add explore & header components

---
+++ src/Explore.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function () {
+	return (
+        <div>
+            <p className="App-intro">
+                To get started, edit <code>src/App.js</code> and save to reload.
+            </p>
+        </div>
+    );
+}
