585629c261fe09298a10eab1b37890a8d5a6bd9c
blshv <awbalashov@gmail.com>
2017-11-08 23:24:36 +0300

    add nav + input

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Explore from './Explore';
+import 'normalize.css';
+import 'flexboxgrid2';
 import './App.css';
 
 class App extends Component {
--- src/Header.js
+++ src/Header.js
@@ -4,9 +4,16 @@ import logo from './airbnbLogo.svg';
 export default function () {
 	return (
 		<header className="header">
-			<div className="row">
-				<img src={logo} className="logo" alt="Airbnb" />
+			<img src={logo} className="logo" alt="Airbnb" />
+			<div className="search">
+				<input placeholder="Try Miami "></input>
 			</div>
+			<nav>
+				<a href="/">Become a host</a>
+				<a href="/">Help</a>
+				<a href="/">Sign Up</a>
+				<a href="/">Log In</a>
+			</nav>
 		</header>
     );
 }
